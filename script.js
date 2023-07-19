69bb6cc215865ea6f6c4ce07d03998f71f9cbc54
Ranjan-coder <ranjanchanda06@gmail.com>
2023-07-19 21:36:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function first_q_1(){
+    console.log(document.getElementById("q_1_ans")).innerHTML = "zero parameter having a console statement"
+}
+
+
+function q_2_ans(a, b){
+    a=4;
+    b=3;
+    console.log(document.getElementById("q_2_ans").innerHTML = ` sum of 4 and 3 is ${a+b}`)
+}
+
